66b8b3cab28fa70027caf1cc2c5508497b7405f0
fidtal24 <talfiderer@gmail.com>
2025-04-30 21:28:41 +0300

    updated version number since uploaded a new readme to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='defaultable',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple Python package for defaultable abstract base classes',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
